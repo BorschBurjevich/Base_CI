aae3fcd258604e6552c379ea8e92d3ae23bbef3b
Nikita Korostelev <korostelev97@bk.ru>
2024-01-14 13:59:36 +0300

    Корректировки в табуляции

--- homework_4/sum_max_min_4.c
+++ homework_4/sum_max_min_4.c
@@ -2,18 +2,18 @@
 // найти сумму максимума и минимума
 int main (void)
 {
-	int a,b,c,d,e,min,max,sum;
-	printf("Enter five integers:\n");
-	scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
-	max = a > b ? a : b;
-	max = max > c ? max : c;
-	max = max > d ? max : d;
-	max = max > e ? max : e;
-	min = a < b ? a : b;
-	min = min < c ? min : c;
-	min = min < d ? min : d;
-	min = min < e ? min : e;
-	sum = min + max;
-	printf("%d\n", sum);
+    int a,b,c,d,e,min,max,sum;
+    printf("Enter five integers:\n");
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    max = a > b ? a : b;
+    max = max > c ? max : c;
+    max = max > d ? max : d;
+    max = max > e ? max : e;
+    min = a < b ? a : b;
+    min = min < c ? min : c;
+    min = min < d ? min : d;
+    min = min < e ? min : e;
+    sum = min + max;
+    printf("%d\n", sum);
     return 0;
-}	
+}
